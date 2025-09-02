2e99249d9be9db7f3c895c76e1e6242655d22700
Kadir49884 <your.email@example.com>
2025-09-02 18:42:19 +0300

    Fix CSP headers for NextAuth Google OAuth

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['fal.media'],
+    domains: ['fal.media', 'v3.fal.media'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'fal.media',
+      },
+      {
+        protocol: 'https',
+        hostname: 'v3.fal.media',
       },
     ],
   },
